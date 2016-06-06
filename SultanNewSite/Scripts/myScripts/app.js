6bdea69b7c89c953f4d3b5da05631d9c79d90c62
gabrySuerz <tsam028@tecnicosuperiorekennedy.it>
2016-06-06 12:17:26 +0200

    new Site started

---
+++ SultanNewSite/Scripts/myScripts/app.js
@@ -0,0 +1,13 @@
+﻿var sultanSite = angular.module('sultanSite', ['ui.router']);
+
+sultanSite.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'htmlPages/home.html',
+            controller: 'homeController'
+        });
+});
