5c0813927c2df8ff8d2b5e185b603ab5219561f9
gabrySuerz <tsam028@tecnicosuperiorekennedy.it>
2016-07-01 15:37:58 +0200

    in prova

--- SultanNewSite/Scripts/myScripts/controller.js
+++ SultanNewSite/Scripts/myScripts/controller.js
@@ -11,7 +11,7 @@ site.controller('mainCtrl', function ($scope, $translate, $rootScope) {
 
     $rootScope.$on('$translateChangeSuccess', function () {
         $scope.image = $translate.instant('TOPBAR.IMAGE');
-    });
+    })
 })
 
 site.controller('homeCtrl', function () {
@@ -30,5 +30,47 @@ site.controller('worksCtrl', function ($translate, stringsJson, $rootscope) {
     $translate.use() === 'it_IT' ? $scope.boats = boatsIT : $scope.boats = boatsEN
     $rootScope.$on('$translateChangeSuccess', function () {
         $translate.use() === 'it_IT' ? $scope.boats = boatsIT : $scope.boats = boatsEN
-    });
+    })
 })
+
+site.controller('insertCtrl', function () {
+    stringsJson.get()
+        .success(function (response) {
+            boatsIT = response.it
+            boatsEN = response.en
+        }).error(function (response) {
+
+        })
+    
+})
+
+function data() {
+    var temp = [
+        { id: 1, name: 'prodotto 1', category: 'A', price: 123.4, description: 'prodotto A' },
+        { id: 2, name: 'prodotto 2', category: 'B', price: 123.4, description: 'prodott B' }
+    ];
+
+    var result = new Object();
+
+    result.getAll = function () {
+        return temp;
+    };
+
+    result.getById = function (id) {
+        for (var i = 0; i < temp.length; i++) {
+            var x = temp[i];
+            if (x.id == id)
+                return x;
+        }
+
+        return null;
+    };
+
+    return result;
+};
+
+var dataAccess = data();
+
+var products = dataAccess.getAll();
+
+var product = dataAccess.getById(1);
--- SultanNewSite/Scripts/myScripts/factory.js
+++ SultanNewSite/Scripts/myScripts/factory.js
@@ -17,3 +17,49 @@
         function getData(url) { return $http.get(url) }
     }
 })();
+
+(function () {
+    'use strict';
+
+    site.factory('insertBoats', factory);
+
+    function factory() {
+        var service = {
+            setData: insertBoats(type, lang, obj)
+        };
+
+        return service;
+
+        function insertBoat(obj) {
+            if (obj != null) {
+                if (boatsArray.length == 0)
+                    obj.id = 1;
+                else
+                    obj.id = boatsArray[boatsArray.length - 1].id + 1;
+                boatsArray.push(obj);
+            }
+        }
+
+        function updateBoat(obj) {
+            if (obj != null) {
+                for (var i = 0; i < boatsArray.length; i++) {
+                    var x = boatsArray[i];
+                    if (x.id == obj.id) {
+                        boatsArray[i] = obj;
+                        return;
+                    }
+                }
+            }
+        }
+
+        function deleteBoat(obj) {
+            for (var i = 0; i < boatsArray.length; i++) {
+                var x = boatsArray[i];
+                if (x.id == id) {
+                    boatsArray.splice(i, 1);
+                    return;
+                }
+            }
+        }
+    }
+})();
